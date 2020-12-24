986cdc4b90caf5c781f8833dc5d30d799444386b
Trent Brew <tbrew1023@gmail.com>
2020-12-24 02:30:58 -0800

    dropdown polish

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -302,23 +302,135 @@ integrated media photography, video and global from-start-to-finish production s
           <div class="services-container-right">
             <ul>
               <li @click="dropdown(0)"><div class="service-icon"></div><span>Strategic Consulting<b></b></span></li>
-              <div class="droppable droppable1" :class="( droppable[0] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable1" :class="( droppable[0] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Brand<span>-</span>Building</span>
+                  <span>Brand<span>-</span>Consulting</span>
+                  <span>Brand<span>-</span>Messaging</span>
+                  <span>Brand<span>-</span>Strategy</span>
+                  <span>Brand<span>-</span>Vision</span>
+                  <span>Content<span>-</span>Planning</span>
+                  <span>Creative<span>-</span>Direction</span>
+                  <span>Digital<span>-</span>Media<span>-</span>Planning</span>
+                  <span>Digital<span>-</span>Strategy</span>
+                  <span>Performance</span>
+                  <span>Marketing<span>-</span>Strategy</span>
+                  <span>Paid<span>-</span>Social<span>-</span>Strategy</span>
+                  <span>Print<span>-</span>+<span>-</span>OOH<span>-</span>Media<span>-</span>Planning</span>
+                  <span>Social<span>-</span>Media<span>-</span>Strategy</span>
+                  <span>Storytelling</span>
+                </div>
+              </div>
               <li @click="dropdown(1)"><div class="service-icon"></div><span>Branding<b></b></span></li>
-              <div class="droppable droppable2" :class="( droppable[1] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable2" :class="( droppable[1] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Art<span>-</span>Direction</span>
+                  <span>Brand<span>-</span>Books<span>-</span>+<span>-</span>Guidelines</span>
+                  <span>Brand<span>-</span>Development</span>
+                  <span>Copy-writing</span>
+                  <span>Graphic<span>-</span>Design</span>
+                  <span>Logo<span>-</span>Design</span>
+                  <span>Name<span>-</span>+<span>-</span>Naming System</span>
+                  <span>Packaging<span>-</span>Design</span>
+                  <span>Storytelling</span>
+                  <span>Visual<span>-</span>Identity</span>
+                  <span>Vision</span>
+                </div>
+              </div>
               <li @click="dropdown(2)"><div class="service-icon"></div><span>Content Creation<b></b></span></li>
-              <div class="droppable droppable3" :class="( droppable[2] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable3" :class="( droppable[2] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Advertising<span>-</span>+<span>-</span>Campaign</span>
+                  <span>Animation</span>
+                  <span>Concept<span>-</span>Development</span>
+                  <span>Creative<span>-</span>Direction</span>
+                  <span>Directing<span>-</span>+<span>-</span>Filming</span>
+                  <span>Editing<span>-</span>+<span>-</span>Post</span>
+                  <span>Global<span>-</span>Production<span>-</span>Services</span>
+                  <span>Illustration</span>
+                  <span>Photography</span>
+                  <span>Retouching</span>
+                  <span>Social<span>-</span>Media<span>-</span>Content</span>
+                  <span>Still<span>-</span>Life<span>-</span>+<span>-</span>Editorial</span>
+                </div>
+              </div>
               <li @click="dropdown(3)"><div class="service-icon"></div><span>Public Relations<b></b></span></li>
-              <div class="droppable droppable4" :class="( droppable[3] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable4" :class="( droppable[3] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Email<span>-</span>Sourcing</span>
+                  <span>Email<span>-</span>Verification</span>
+                  <span>Creative<span>-</span>Email<span>-</span>Marketing</span>
+                  <span>Development<span>-</span>Support</span>
+                  <span>Email<span>-</span>Template<span>-</span>Creation</span>
+                </div>
+              </div>
               <li @click="dropdown(4)"><div class="service-icon"></div><span>Talent<b></b></span></li>
-              <div class="droppable droppable5" :class="( droppable[4] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable5" :class="( droppable[4] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Calabrity<span>-</span>Seeding</span>
+                  <span>Infuencer<span>-</span>Gifting</span>
+                  <span>Infulencer<span>-</span>Partnerships</span>
+                  <span>Strategic<span>-</span>Collaborations</span>
+                  <span>Strategic<span>-</span>Partnerships</span>
+                  <span>Talent<span>-</span>Campaign Strategy</span>
+                  <span>Talent<span>-</span>Campaign<span>-</span>Global<span>-</span>Execution</span>
+                </div>
+              </div>
               <li @click="dropdown(5)"><div class="service-icon"></div><span>Email<b></b></span></li>
-              <div class="droppable droppable6" :class="( droppable[5] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable6" :class="( droppable[5] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Digital<span>-</span>+<span>-</span>Traditional Press</span>
+                  <span>Fashion<span>-</span>Shows</span>
+                  <span>Front<span>-</span>of<span>-</span>House</span>
+                  <span>Full-event<span>-</span>Production</span>
+                  <span>Openings</span>
+                  <span>Planning</span>
+                  <span>Presentations</span>
+                  <span>PR<span>-</span>Box<span>-</span>/<span>-</span>Mailers</span>
+                  <span>Product<span>-</span>Launches</span>
+                  <span>Top-tier<span>-</span>Editorial<span>-</span>Placement</span>
+                </div>
+              </div>
               <li @click="dropdown(6)"><div class="service-icon"></div><span>Social Media<b></b></span></li>
-              <div class="droppable droppable7" :class="( droppable[6] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable7" :class="( droppable[6] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Benchmarking<span>-</span>&<span>-</span>Analysis</span>
+                  <span>Brand<span>-</span>Social<span>-</span>Identity</span>
+                  <span>Community<span>-</span>Building</span>
+                  <span>Community<span>-</span>Management</span>
+                  <span>Social<span>-</span>Media<span>-</span>Growth<span>-</span>Giveaways</span>
+                  <span>Paid<span>-</span>Social<span>-</span>Framework</span>
+                  <span>Social<span>-</span>Aesthetic<span>-</span>Appeal</span>
+                  <span>Social<span>-</span>Media<span>-</span>Management</span>
+                  <span>Social<span>-</span>Media<span>-</span>Strategy</span>
+                  <span>Strategic<span>-</span>Alignment</span>
+                </div>
+              </div>
               <li @click="dropdown(7)"><div class="service-icon"></div><span>Spacial Design<b></b></span></li>
-              <div class="droppable droppable8" :class="( droppable[7] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable8" :class="( droppable[7] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>3D<span>-</span>Rendering</span>
+                  <span>Custom<span>-</span>Wall<span>-</span>Graphics</span>
+                  <span>In-store<span>-</span>Displays</span>
+                  <span>Large<span>-</span>Space<span>-</span>Graphics</span>
+                  <span>Retail<span>-</span>Displays</span>
+                  <span>Special<span>-</span>Events<span>-</span>&<span>-</span>Booth<span>-</span>Design</span>
+                  <span>Vector<span>-</span>Graphics</span>
+                </div>
+              </div>
               <li @click="dropdown(8)"><div class="service-icon"></div><span>Additional Services<b></b></span></li>
-              <div class="droppable droppable9" :class="( droppable[8] == true ? 'dropped' : 'collapsed' )"></div>
+              <div class="droppable droppable9" :class="( droppable[8] == true ? 'dropped' : 'collapsed' )">
+                <div class="bubble-items">
+                  <span>Advisory<span>-</span>Board<span>-</span>Assembly</span>
+                  <span>Celebrity<span>-</span>Giveaways</span>
+                  <span>CMO<span>-</span>on<span>-</span>Demand</span>
+                  <span>Digital<span>-</span>Ads<span>-</span>Management</span>
+                  <span>Growth<span>-</span>Hacking</span>
+                  <span>Marketing<span>-</span>Advising</span>
+                  <span>SEO<span>-</span>-<span>-</span>SEM</span>
+                  <span>Social<span>-</span>Verification</span>
+                </div>
+              </div>
             </ul>
           </div>
         </div>
@@ -368,6 +480,33 @@ $pageHeight: calc(100vh - 80px);
   margin-bottom: 120px;
   display: flex;
 
+  .bubble-items {
+    text-align: left;
+    width: 80%;
+    margin-left: 24px;
+    margin-top: 4px;
+    transition: 300ms;
+
+    span {
+      margin-right: 4px;
+      color: black;
+      background: #eee;
+      padding: 4px 8px;
+      line-height: 1;
+      font-size: 12px;
+      border-radius: 12px;
+      display:inline-block;
+      margin-bottom: 6px;
+      font-weight: bold;
+
+      span {
+        padding: 0px;
+        margin: 0px;
+        color: transparent;
+      }
+    }
+  }
+
   ul {
     margin: 0px;
     padding: 0px;
@@ -378,16 +517,33 @@ $pageHeight: calc(100vh - 80px);
       height: 0px;
       //background: lime;
       transition: 300ms;
+      pointer-events: none;
     }
 
     .dropped {
       //background: pink;
       height: 200px;
+      pointer-events: none;
+      //background: green;
+
+      .bubble-items {
+        transition-delay: 100ms;
+        //background: pink;
+        opacity: 1;
+      }
     }
 
     .collapsed {
       //background: gray;
       height: 0px;
+      //opacity: 0%;
+      pointer-events: none;
+      
+      .bubble-items {
+        //background: red;
+        opacity: 0;
+        transition: 100ms;
+      }
     }
     
     li {
@@ -418,7 +574,13 @@ $pageHeight: calc(100vh - 80px);
       }
 
       &:hover {
-        background: #eee;
+        span {
+          color: gray;
+
+          b {
+            color: black !important;
+          }
+        }
       }
     }
   }
